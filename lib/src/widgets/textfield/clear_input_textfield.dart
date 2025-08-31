a68fcde567de6b0685f12c1cc9541d82703e6117
shan <1072818264@qq.com>
2025-08-31 12:05:52 +0800

    feat: 1、调整输入框组件的背景

--- lib/src/widgets/textfield/clear_input_textfield.dart
+++ lib/src/widgets/textfield/clear_input_textfield.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'app_textfield.dart';
+import 'package:ruolanui/ruolanui.dart';
 
 class ClearInputTextField extends StatefulWidget {
   final bool enable;
@@ -15,36 +14,32 @@ class ClearInputTextField extends StatefulWidget {
   final TextInputType keyboardType;
   final TextInputAction? textInputAction;
   final FocusNode? focusNode;
-  final InputBorder? border;
+  final double borderRadius;
   final Color? fillColor;
   final bool autoFocus;
   final ValueChanged<String>? onSubmitted;
 
-  const ClearInputTextField({
-    super.key,
-    this.enable = true,
-    required this.hintText,
-    this.icon,
-    this.controller,
-    this.value,
-    required this.onChange,
-    this.filterPattern,
-    this.textInputAction,
-    this.focusNode,
-    this.tailIcon,
-    this.fillColor,
-    this.onSubmitted,
-    bool? autoFocus,
-    bool? obscureText,
-    InputBorder? border,
-    TextInputType? keyboardType,
-  }) : obscureText = obscureText ?? false,
-       keyboardType = keyboardType ?? TextInputType.text,
-       border = const OutlineInputBorder(
-         borderRadius: BorderRadius.all(Radius.circular(12)),
-         borderSide: BorderSide.none,
-       ),
-       autoFocus = autoFocus ?? false;
+  const ClearInputTextField(
+      {super.key,
+      this.enable = true,
+      required this.hintText,
+      this.icon,
+      this.controller,
+      this.value,
+      required this.onChange,
+      this.filterPattern,
+      this.textInputAction,
+      this.focusNode,
+      this.tailIcon,
+      this.fillColor,
+      this.onSubmitted,
+      bool? autoFocus,
+      bool? obscureText,
+      this.borderRadius = 12,
+      TextInputType? keyboardType})
+      : obscureText = obscureText ?? false,
+        keyboardType = keyboardType ?? TextInputType.text,
+        autoFocus = autoFocus ?? false;
 
   @override
   State<ClearInputTextField> createState() => _ClearInputTextFieldState();
@@ -112,7 +107,10 @@ class _ClearInputTextFieldState extends State<ClearInputTextField> {
           icon: const Icon(Icons.clear),
         ),
       ),
-      border: widget.border,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(widget.borderRadius)),
+        borderSide: BorderSide.none,
+      ),
       onChange: widget.onChange,
       focusNode: _focusNode,
       controller: _controller,
