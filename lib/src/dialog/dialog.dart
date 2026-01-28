1bd1e9fe1ce79c6ab755e53c622481406d7d842c
shan <1072818264@qq.com>
2026-01-28 22:47:09 +0800

    feat: 新增简单的选项选择器

--- lib/src/dialog/dialog.dart
+++ lib/src/dialog/dialog.dart
@@ -21,3 +21,23 @@ Future<Result<int>> showOptionsDialog(
 
   return result ?? Result.failure("Cancel");
 }
+
+Future<Result<int>> showSampleOptionsDialog(
+    {required BuildContext context,
+    required List<String> options,
+    int? value,
+    String cancelText = "取消"}) async {
+  final newOptions = List.generate(
+      options.length, (i) => OptionItem(id: i, label: options[i]));
+  final result = await showModalBottomSheet<Result<int>>(
+      context: context,
+      backgroundColor: Colors.transparent,
+      isScrollControlled: true,
+      builder: (c) => OptionsContent(
+            options: newOptions,
+            value: value,
+            cancelText: cancelText,
+          ));
+
+  return result ?? Result.failure("Cancel");
+}
