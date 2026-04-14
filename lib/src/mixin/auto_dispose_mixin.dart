5d5996377a27b2d229cd44bfffff792a69f292fa
shan <1072818264@qq.com>
2026-04-14 22:05:43 +0800

    feat: auto_dispose_mixin.dart新增自动注销stream

--- lib/src/mixin/auto_dispose_mixin.dart
+++ lib/src/mixin/auto_dispose_mixin.dart
@@ -1,10 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/widgets.dart';
 
 /// 自动 dispose mixin，用于管理需要在 State dispose 时清理的资源。
 ///
-/// 使用 [register] 方法注册任意 [ChangeNotifier] 子类（如 FocusNode、
+/// 使用 [autoDispose] 方法注册任意 [ChangeNotifier] 子类（如 FocusNode、
 /// TextEditingController、ObservableTextController 等），
-/// 在 State dispose 时自动调用其 dispose()。
+/// 使用 [autoCancel] 方法注册 [StreamSubscription]，
+/// 在 State dispose 时自动清理。
 ///
 /// ```dart
 /// class _MyPageState extends State<MyPage>
@@ -13,10 +16,12 @@ import 'package:flutter/widgets.dart';
 ///   late final ObservableTextController _controller = autoDispose(
 ///     ObservableTextController(...),
 ///   );
+///   late final StreamSubscription _sub = autoCancel(someStream.listen(...));
 /// }
 /// ```
 mixin AutoDisposeMixin<T extends StatefulWidget> on State<T> {
   final List<ChangeNotifier> _disposeList = [];
+  final List<StreamSubscription> _streamList = [];
 
   /// 注册一个 [ChangeNotifier]，在 State dispose 时自动 dispose。
   /// 返回传入的对象本身，方便直接赋值。
@@ -25,12 +30,23 @@ mixin AutoDisposeMixin<T extends StatefulWidget> on State<T> {
     return notifier;
   }
 
+  /// 注册一个 [StreamSubscription]，在 State dispose 时自动 cancel。
+  /// 返回传入的对象本身，方便直接赋值。
+  S autoCancel<S extends StreamSubscription>(S subscription) {
+    _streamList.add(subscription);
+    return subscription;
+  }
+
   @override
   void dispose() {
     for (final notifier in _disposeList) {
       notifier.dispose();
     }
     _disposeList.clear();
+    for (final sub in _streamList) {
+      sub.cancel();
+    }
+    _streamList.clear();
     super.dispose();
   }
 }
